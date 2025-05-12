8abbdeb750733ce263ffdd185ec442a2529007ef
denusbtw <denisrozum@gmail.com>
2025-05-12 08:22:00 +0300

    fix

--- lab5/backend/firebaseAdmin.js
+++ lab5/backend/firebaseAdmin.js
@@ -1,5 +1,14 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+let serviceAccount;
+
+try {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} catch (err) {
+    console.error('❌ Firebase key parsing error:', err);
+    process.exit(1);
+}
+
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
